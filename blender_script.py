f8648354318da6b62d6cb7faadaf01319e9a4a49
AJSVB <antoine.scardigli@epfl.ch>
2022-08-27 16:00:23 +0200

    first commit

---
+++ blender_script.py
@@ -0,0 +1,17 @@
+import bpy
+import time
+from random import randint
+
+
+scene = bpy.context.scene
+for frame in range(scene.frame_start, scene.frame_start + 1):
+  for i in range(920,1000):
+    bpy.context.scene.cycles.seed = i 
+    bpy.context.scene.render.image_settings.color_mode ='RGB'
+    bpy.context.scene.render.image_settings.file_format='PNG'
+    bpy.context.scene.render.filepath = "~"  
+    scene.render.filepath = '~/' + str(frame).zfill(4) + "-" + str(i).zfill(5) 
+    scene.frame_set(frame)
+    bpy.ops.render.render(write_still=True)
+  
+
