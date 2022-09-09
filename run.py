3c683fa240e73b8ddadc4e8da93f19f0743658c2
AJSVB <scardigliantoine@gmail.com>
2022-09-09 17:49:03 +0200

    it works now

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import unet
+#import unet
 import fcn
 import simulation 
 import ray
@@ -21,13 +21,13 @@ def train_ppo_model():
 #"eager_tracing":True,
 #"clip_param":.07,"decay":.97,"epsilon":.01, "grad_clip":.4,"lambda":.925,"lr":.001, "momentum":.1,"vf_loss_coeff":.81,"entropy_coeff":1e-4,
 "num_envs_per_worker":1,
-        'num_workers':2,
+        'num_workers':4,
 #"evaluation_num_workers":1,
 #'num_cpus_per_worker':10,
-'num_gpus_per_worker':2,
+'num_gpus_per_worker':1,
 "evaluation_interval":5,
-"rollout_fragment_length":20, #Increase this
-"train_batch_size":40,
+"rollout_fragment_length":15, #Increase this
+"train_batch_size":60,
 "replay_buffer_num_slots":50,
 #"disable_env_checking":True,
   "model":{
