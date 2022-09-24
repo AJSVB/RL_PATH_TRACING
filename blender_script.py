fc39755eb9877f2a7118b560aabd5c2c2f785855
AJSVB <scardigliantoine@gmail.com>
2022-09-24 23:25:08 +0200

    pul

--- blender_script.py
+++ blender_script.py
@@ -4,25 +4,26 @@ from random import randint
 
 nodes = bpy.context.scene.node_tree.nodes
 render_layers = nodes.new('CompositorNodeRLayers')
-"""
+scene = bpy.context.scene
+
+
 links = bpy.context.scene.node_tree.links
 render_layers = nodes.new('CompositorNodeRLayers')
 scene = bpy.context.scene
 albedo_file_output = nodes.new(type="CompositorNodeOutputFile")
 albedo_file_output.label = 'Albedo Output'
-albedo_file_output.base_path = "" 
+albedo_file_output.base_path = "/home/ascardigli/datasets/temple/" 
 albedo_file_output.file_slots[0].use_node_format = True
 albedo_file_output.format.file_format = 'PNG'
 albedo_file_output.format.color_mode = 'RGB'
 albedo_file_output.format.color_depth = '16'
 links.new(render_layers.outputs["Image"], albedo_file_output.inputs[0])
-"""
 
 for frame in range(scene.frame_start, scene.frame_start + 1):
   for i in range(1000,10000):
     bpy.context.scene.cycles.seed = i 
     scene.frame_set(frame)
-    albedo_file_output.file_slots[0].path = '~/' + str(frame).zfill(4) + "-" + str(i).zfill(5)+ ".png" 
+    albedo_file_output.file_slots[0].path =  str(frame).zfill(4) + "-" + str(i).zfill(5)+ ".png" 
     bpy.ops.render.render(write_still=True)
   
 
