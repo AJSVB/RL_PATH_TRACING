63659dcf3d91332b2b18754adc9df03cac554457
AJSVB <scardigliantoine@gmail.com>
2022-09-26 12:34:23 +0200

    restore

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 #import unet
 import fcn
 import ray
@@ -8,21 +9,16 @@ import ray.rllib.algorithms.ppo as ppo
 import ray.rllib.algorithms.ddppo as ddppo
 
 import ray.rllib.algorithms.appo as appo
-
-import ray.rllib.algorithms.sac as sac #Needs a policy and a q model
-import ray.rllib.algorithms.td3 as td3 #Gave a mistake (strnage)
-import ray.rllib.algorithms.apex_ddpg as apex #Same mistake
-
 import random
 
 def train_ppo_model():
     a = time.time()
     algo = appo.APPO(env=simulation.CustomEnv,config={
 'env_config':{'path': "../datasets/temple/",'number_images':None,\
-'frame_number':1, 'spp':2, "sppps":.1,"denoising":True,"prob_sampling":True,
+'frame_number':1, 'spp':2, "sppps":.1,"denoising":True,
             },
           'framework' :"torch",
-
+#"eager_tracing":True,
 "clip_param":.45,"decay":.97,"epsilon":.01, "grad_clip":4,"lambda":.925,"lr":.0001, "momentum":.5,"vf_loss_coeff":.81,"entropy_coeff":1e-3,
 "num_envs_per_worker":1,
         'num_workers':4,
@@ -33,12 +29,14 @@ def train_ppo_model():
 "rollout_fragment_length":20, #Increase this
 "train_batch_size":80,
 "replay_buffer_num_slots":50,
+#"disable_env_checking":True,
   "model":{
+#"conv_activation":"tanh"
    "custom_model":"FCN"
 }
 })
     # Train for one iteration.
-    for _ in range(100):
+    for _ in range(30):
          algo.train()
     print(time.time()-a)
     # Save state of the trained Algorithm in a checkpoint.
