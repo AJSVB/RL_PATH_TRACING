fc78d40056d163156bd3322d0f9a339fe4fdf840
AJSVB <scardigliantoine@gmail.com>
2022-10-04 17:04:06 +0200

    more or less most advance deisgn, no more errors except the nan exception

---
+++ fcn.py
@@ -0,0 +1,136 @@
+
+import GPUtil
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms.functional as TF
+import time
+import torchvision.transforms as T
+import random
+import numpy as np
+from typing import Dict, List
+import gym
+
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.torch.misc import (
+    normc_initializer,
+    same_padding,
+    SlimConv2d,
+    SlimFC,
+)
+
+from torch import nn 
+
+from ray.rllib.models.utils import get_activation_fn, get_filter_config
+from ray.rllib.utils.annotations import override
+from ray.rllib.utils.framework import try_import_torch
+from ray.rllib.utils.typing import ModelConfigDict, TensorType
+
+torch, nn = try_import_torch()
+
+
+class FCN(TorchModelV2, nn.Module):
+    def __init__(
+        self,
+        obs_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        num_outputs: int,
+        model_config: ModelConfigDict,
+        name: str,
+    ):
+        c=11
+        model_config["conv_filters"] = [
+            #                            [12,[c,c], [1,1]],
+            #                            [12,[c,c], [1,1]],
+           #                             [12,[c,c], [1,1]],
+           #                             [12,[c,c], [1,1]],
+           #                             [12,[c,c], [1,1]],
+           #                             [10,[c,c], [1,1]],
+                                        [20,[c,c], [1,1]],
+                                        [14,[c,c], [1,1]],
+                                        [8,[c,c], [1,1]],
+                                        [1,[c,c], [1,1]],
+                                        [1,[c,c], [1,1]],
+                                        [1,[c,c], [1,1]]]
+
+        TorchModelV2.__init__(
+            self, obs_space, action_space, num_outputs, model_config, name
+        )
+        nn.Module.__init__(self)
+
+        filters = model_config["conv_filters"]
+
+        layers = []
+        (in_channels,w, h) = obs_space.shape
+        in_size = [w, h]
+        for out_channels, kernel, stride in filters:
+            padding, out_size = same_padding(in_size, kernel, stride)
+            layers.append(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel,
+                    stride,
+                    padding[:2],
+                #    activation_fn=activation,
+                )
+            )
+            layers.append(nn.Tanh())
+            in_channels = out_channels
+            in_size = out_size
+
+        self._convs = nn.Sequential(*layers[:-4])#.cuda(0)
+        self.head = nn.Sequential(*layers[-2:0])#.cuda(0)
+        self.head_value = nn.Sequential(*layers[-4:-2])#.cuda(0)
+        #GPUtil.showUtilization()
+
+    def f(
+        self,
+        y,
+        state: List[TensorType],
+        seq_lens: TensorType,
+    ) -> (TensorType, List[TensorType]):
+        CST=1
+        for i in range(int(y.shape[0]/CST)):
+         x = y[CST*i:CST*(i+1),]
+         x = self._convs(x)
+         tmp = self.head_value(x).reshape(*x.shape[:1],-1).mean(1)
+         o=self.head(x)
+         if i==0:
+          self.tmp = tmp
+          out=o
+         else:
+          out=torch.cat((out,o),0)
+          self.tmp=torch.cat((self.tmp,tmp),0)
+        return out
+
+
+    @override(TorchModelV2)
+    def forward(
+        self,
+        input_dict: Dict[str, TensorType],
+        state: List[TensorType],
+        seq_lens: TensorType,
+    ) -> (TensorType, List[TensorType]):
+      x=input_dict["obs"]
+      if x.shape[0]==8:
+       with torch.no_grad():
+          out=self.f(x,state,seq_lens)
+      else:
+          out=self.f(x,state,seq_lens)
+      #print(out[0].cpu().reshape(-1).detach().numpy())
+      #print(out[1].cpu().reshape(-1).detach().numpy())
+      t = out * 0 -10  
+      out=torch.cat((out,t),1)
+      out = out.reshape(input_dict["obs"].shape[0], -1)
+      return  out, state
+
+    @override(TorchModelV2)
+    def value_function(self) -> TensorType:
+        assert self.tmp is not None, "must call forward() first"
+        return self.tmp
+
+
+    
+from ray.rllib.models import ModelCatalog
+ModelCatalog.register_custom_model("FCN", FCN)
---
+++ run.py
@@ -0,0 +1,50 @@
+
+import fcn
+import ray
+import simulation 
+import time
+
+import ray.rllib.algorithms.ppo as ppo
+import ray.rllib.algorithms.ddppo as ddppo
+
+import ray.rllib.algorithms.appo as appo
+
+import ray.rllib.algorithms.sac as sac #Needs a policy and a q model
+import ray.rllib.algorithms.td3 as td3 #Gave a mistake (strnage)
+import ray.rllib.algorithms.apex_ddpg as apex #Same mistake
+
+import random
+
+def train_ppo_model():
+    a = time.time()
+    algo = appo.APPO(env=simulation.CustomEnv,config={
+'env_config':{'path': "../datasets/temple/",'number_images':None,\
+'frame_number':1, 'spp':2, "sppps":.1,"denoising":True,"prob_sampling":True,"partition":[0]
+            },
+          'framework' :"torch",
+
+#"clip_param":.45,"decay":.97,"epsilon":.01, "grad_clip":4,"lambda":.925,"lr":.0001, "momentum":.5,"vf_loss_coeff":.81,"entropy_coeff":1e-3,
+"num_envs_per_worker":2,
+        'num_workers':1,
+#"evaluation_num_workers":1,
+#'num_cpus_per_worker':10,
+'num_gpus_per_worker':4,
+"evaluation_interval":5,
+"rollout_fragment_length":4, #was20
+"train_batch_size":4,
+#"replay_buffer_num_slots":20,
+  "model":{
+   "custom_model":"FCN"
+}
+})
+    # Train for one iteration.
+    for _ in range(2):
+         algo.train()
+    print(time.time()-a)
+    # Save state of the trained Algorithm in a checkpoint.
+   # algo.save("/tmp/rllib_checkpoint")
+   # return "/tmp/rllib_checkpoint/checkpoint_000001/checkpoint-1"
+
+
+#simulation.ground_truth("../datasets/temple/",name="truth.png")
+train_ppo_model()
