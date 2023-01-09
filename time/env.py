66b398ae1dee181844b56af5e3c9964dc3a4c3e4
AJSVB <scardigliantoine@gmail.com>
2023-01-09 15:40:56 +0100

    cleaned

--- time/env.py
+++ time/env.py
@@ -1,43 +1,32 @@
-from physicalsimulation import PhysicSimulation
-import torchvision
-import torchvision.transforms as transforms
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from PIL import Image
-import torchvision.transforms.functional as TF
 import time
-import torchvision.transforms as T
-import torch
-import unet1
-from filelock import FileLock 
-from training import  train
+
+import gym
 import matplotlib.pyplot as plt
+import numpy as np
+import ray
+import torch
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
+from filelock import FileLock
+from gym import Env, spaces
+from PIL import Image
+from pytorch_msssim import MS_SSIM, SSIM, ms_ssim, ssim
+from torch.utils.data import Dataset
 from torchmetrics import PeakSignalNoiseRatio
 from torchmetrics.functional import mean_squared_error
-L=1
-psnr = PeakSignalNoiseRatio().cuda(0)
-
-def p(x,y):
-   pass
-
-list = []
-
-from torchvision.transforms import *
-
 from torchvision.transforms import functional as F
+
+from physicalsimulation import PhysicSimulation
+from training import train
+
+psnr = PeakSignalNoiseRatio().cuda(0)
 
 class Spec:
    def __init__(self,max_episode_steps):
     self.max_episode_steps = max_episode_steps
     self.id = "3Drenderingenv"
-
-from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
-
-import ray
-
-import gym
-from gym import Env, spaces
-import numpy as np
 
 class CustomEnv(gym.Env):
   metadata = {'render.modes': ['human']}
@@ -45,12 +34,12 @@ class CustomEnv(gym.Env):
     super(CustomEnv, self).__init__()
     self.spp = env_config['spp']
     self.sppps = env_config["sppps"]
+    self.mode = env_config["mode"]
     self.HEIGHT = 720 
     self.WIDTH =   720
-
     self.max = 20
+
     out_space=41
-    self.mode = env_config["mode"]
     if self.mode == "ntas":
      self.model,self.data,self.criterion,self.optimizer,self.scheduler = train.main_worker(12,3)
      out_space=12
@@ -61,42 +50,40 @@ class CustomEnv(gym.Env):
      self.model,self.data,self.criterion,self.optimizer,self.scheduler = train.main_worker(12,0)
      out_space=9
     else:
-
      self.model,self.data,self.criterion,self.optimizer,self.scheduler = train.main_worker()
 
     self.model=self.model.to("cuda:0")
     self.criterion = self.criterion.to("cuda:0")
-
-
     self.offset=0
-    self.simulation = PhysicSimulation(self.spp,self.sppps,self.HEIGHT,self.WIDTH,self,self.offset,self.mode)
+
+    self.simulation = PhysicSimulation(self)
     self.action_space = spaces.Box(low=0,high=1,shape=(int(self.HEIGHT*self.WIDTH),))
     self.observation_space = spaces.Box(low=-1.0001, high=1.0001, shape=
                     (out_space,int(self.HEIGHT),int(self.WIDTH)), dtype=np.float32) #MACHINE PRECISION
+
     self.spec = Spec(self.max)
     self.top=0
     self.a=time.time()
     self.mses = []
     self.psnrs = []
-
+    self.sum=0
+    self.cntr=0
     with open("comp/"+str(self.spp)+'mses'+self.mode+'.txt', 'w') as fp:
         fp.write("\n")
     with open("comp/"+str(self.spp)+'psnrs'+self.mode+'.txt', 'w') as fp:
         fp.write("\n")
 
-    self.time=time.time()
-    self.sum=0
-    self.cntr=0
-
-
-  def step(self, action):
-    self.sum+=time.time() - self.time
-    self.time= time.time()
+  def time(self)
+    self.sum+=time.time() - self.a
+    self.a= time.time()
     self.cntr+=1
     if self.cntr ==1000:
       print(self.sum/1000)
       self.cntr=0
       self.sum=0   
+
+  def step(self, action):
+    self.time()
 
     self.simulation.new(self.simulation.count)
     self.simulation.simulate(action)
@@ -107,8 +94,7 @@ class CustomEnv(gym.Env):
 
     if  self.bool:
      te = str((self.simulation.offset%100)+self.simulation.count)
-     save(new.cpu(),"images/"+str(self.spp)+"new"+te+"_"+str(self.mode)+".png")
-  
+     save(new.cpu(),"images/"+str(self.spp)+"new"+te+"_"+str(self.mode)+".png")  
      self.mses.append(mean_squared_error(new,gd).cpu())
      self.psnrs.append(psnr(new,gd).cpu())
 
@@ -116,15 +102,11 @@ class CustomEnv(gym.Env):
     done = self.spec.max_episode_steps <= self.simulation.count
     return observation.numpy(),reward.detach().numpy(),done,{}
  
-  def insight(self): 
-    img= self.simulation.s.reshape(self.HEIGHT,self.WIDTH,1)*1./np.max(self.simulation.s)
-    te=str(self.top.item())
-    save(img.astype(np.float32),"images/"+te+".png")
   def reset(self):
     self.bool=False
     if self.offset%self.simulation.number >= 800 and self.offset%self.simulation.number<900 : #was between 800 and 900
       self.bool=True
-    self.simulation = PhysicSimulation(self.spp,self.sppps,self.HEIGHT,self.WIDTH,self,int((self.offset//20)*20) %self.simulation.number,self.mode)
+    self.simulation = PhysicSimulation(self)
     temp ,_= self.simulation.observe()
 
     if self.bool:
--- time/physicalsimulation.py
+++ time/physicalsimulation.py
@@ -1,4 +1,3 @@
-import math
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
@@ -20,21 +19,15 @@ from torch.autograd import Function
 from torch.autograd import Variable
 import random
 from torchvision.transforms import *
-
 from torchvision.transforms import functional as F
 
 
 def nostate(st):
   return "notp" in st or "dasr"==st
 
-L=1
-psnr = PeakSignalNoiseRatio().cuda(0)
-
-
 class Render(Function):
     @staticmethod
     def forward(ctx, x, sim):
-#        x = x - torch.min(x)
         x=torch.flatten(x)
         M = sim.sppps*sim.WIDTH*sim.HEIGHT
         e = torch.exp(x)
@@ -53,7 +46,7 @@ class Render(Function):
         obs = obs.reshape(3,720,720)
 
         ctx.save_for_backward((sim.gd - obs)/s.reshape(1,720,720).expand(3,-1,-1))
-        if sim.mode=="grad":
+        if sim.mode!="ntas" and sim.mode!="dasr":
          return observations
         else:
          return obs
@@ -68,72 +61,16 @@ class Render(Function):
         return tuple([dL_din] + [None]*7)
 
 
-
-
-
-
-def p(x,y):
-   pass
-#   print(x+str(y))
-
-#L1 = torch.nn.L1Loss()
-
-list = []
-
-import random
-from torchvision.transforms import *
-
-from torchvision.transforms import functional as F
-
-size=(720,720)
-scale=(0.08, 1.0)
-ratio=(3.0 / 4.0, 4.0 / 3.0)
-interpolation=InterpolationMode.BILINEAR
-
-def get_params():
-        height, width = 720,720 
-        area = height * width
-        log_ratio = torch.log(torch.tensor(ratio))
-        for _ in range(10):
-            target_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
-            aspect_ratio = torch.exp(torch.empty(1).uniform_(log_ratio[0], log_ratio[1])).item()
-            w = int(round(math.sqrt(target_area * aspect_ratio)))
-            h = int(round(math.sqrt(target_area / aspect_ratio)))
-            if 0 < w <= width and 0 < h <= height:
-                i = torch.randint(0, height - h + 1, size=(1,)).item()
-                j = torch.randint(0, width - w + 1, size=(1,)).item()
-                return i, j, h, w
-        in_ratio = float(width) / float(height)
-        if in_ratio < min(ratio):
-            w = width
-            h = int(round(w / min(ratio)))
-        elif in_ratio > max(ratio):
-            h = height
-            w = int(round(h * max(ratio)))
-        else:  # whole image
-            w = width
-            h = height
-        i = (height - h) // 2
-        j = (width - w) // 2
-        return i, j, h, w
-
 class PhysicSimulation:
-
-    def __init__(self,spp, sppps=.1,HEIGHT=480,WIDTH=730,sel=None,offset=0,mode=""):
-        self.model,self.data,self.criterion,self.optimizer,self.scheduler = \
-sel.model,sel.data,sel.criterion,sel.optimizer,sel.scheduler
-        self.mode = mode
-        self.HEIGHT =  HEIGHT
-        self.WIDTH =   WIDTH
-        self.sppps = sppps
+    def __init__(self,sel):
         self.number = 1200
-        self.offset = offset
+
+        self.spp,self.sppps,self.HEIGHT,self.WIDTH,self.offset,self.mode = sel.spp,sel.sppps,sel.HEIGHT,sel.WIDTH,int((sel.offset//20)*20) %self.number,sel.mode 
+        self.model,self.data,self.criterion,self.optimizer,self.scheduler = sel.model,sel.data,sel.criterion,sel.optimizer,sel.scheduler
+
         self.reset()
         self.new(-1)
-        self.shape= [8,720,720]
-        self.loss=0
-        self.x=1
-        self.y=1
+        self.shape= [8,self.HEIGHT,self.WIDTH]
 
     def reset(self):
         self.observations = -1 * torch.ones([8,3,self.HEIGHT,self.WIDTH])
@@ -150,20 +87,14 @@ sel.model,sel.data,sel.criterion,sel.optimizer,sel.scheduler
         self.perm = lambda x:x
         if random.random()>.5:
          lis.append(T.functional.hflip)
-         self.x=-1
         if random.random()>.5:
          lis.append(T.functional.vflip)
-         self.y=-1
-        if random.random()>1.5:
-         i, j, h, w = get_params()
-         self.perm = lambda x: F.resized_crop(x, i, j, h, w,size, interpolation)
         if self.inval():
          lis=[]
-         self.perm = lambda x:x
         self.transform = T.Compose(lis)
 
     def new(self,i):
-        transform = lambda x: self.perm(self.transform(x))
+        transform = lambda x: self.transform(x)
         if i ==-1:
          self.nextadd, self.gd = self.data.get(i+1+self.offset)
          self.nextadd = transform(torch.Tensor(self.nextadd).permute(2,0,1).cuda(0))
